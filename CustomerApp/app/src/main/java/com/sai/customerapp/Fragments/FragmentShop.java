77a253d1412ea2248c83dfabb9dba5cbb35156e5
albertusk95 <albertusk15@gmail.com>
2017-04-10 01:05:01 +0700

    add SHOP fragment for recycler view AND SHOP fragment for adapter layout

--- CustomerApp/app/src/main/java/com/sai/customerapp/Fragments/FragmentShop.java
+++ CustomerApp/app/src/main/java/com/sai/customerapp/Fragments/FragmentShop.java
@@ -1,10 +1,77 @@
 package com.sai.customerapp.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.sai.customerapp.R;
+
+import java.util.ArrayList;
 
 public class FragmentShop extends Fragment {
 
-    public FragmentShop() {
+    View v;
+    WebView myWeb;
+
+    //recyclerview object
+    private RecyclerView recyclerView;
+
+    //adapter object
+    private RecyclerView.Adapter adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+
+        // menambahkan layout
+        v = inflater.inflate(R.layout.fragment_shop_recyclerView, container, false);
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        tenantInfo = new ArrayList<>();
+
+        return v;
+
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //you can set the title for your toolbar here for different fragments different titles
+        //getActivity().setTitle("All Reports");
+
+        // retrieve report info from database
+        retrieveShopInfo();
+    }
+
+
+    private void retrieveShopInfo() {
+
+        // [TO-DO]
+        // SEND POST REQUEST TO SERVER FOR THE LIST OF TENANT
+
+
+        // [EXPERIMENT]
+
+        // creating list of tenantInfo
+        tenantInfo = new ArrayList<>();
+
+        // creating tenantInfo object
+        TenantInfo tenantInfo = new TenantInfo();
+
+        // creating adapter
+        adapter = new ShopAdapter(getActivity().getApplicationContext(), tenantInfo);
+
+        // adding adapter to recyclerview
+        recyclerView.setAdapter(adapter);
 
     }
 
