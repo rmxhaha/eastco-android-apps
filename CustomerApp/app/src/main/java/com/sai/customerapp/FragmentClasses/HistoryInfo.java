0a56a05709b485abf1c85590a8312ea36abfd416
albertusk95 <albertusk15@gmail.com>
2017-04-10 18:29:34 +0700

    add History Info (POJO class)

---
+++ CustomerApp/app/src/main/java/com/sai/customerapp/FragmentClasses/HistoryInfo.java
@@ -0,0 +1,20 @@
+package com.sai.customerapp.FragmentClasses;
+
+/**
+ * Created by AlbertusK95 on 4/10/2017.
+ */
+
+public class HistoryInfo {
+
+    public String historyMenuName;
+    public String historyAddress;
+    public int historyTotalPrice;
+
+    public HistoryInfo(String historyMenuName, String historyAddress, int historyTotalPrice) {
+        this.historyMenuName = historyMenuName;
+        this.historyAddress = historyAddress;
+        this.historyTotalPrice = historyTotalPrice;
+    }
+
+
+}
